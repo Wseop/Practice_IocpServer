20022af2bec7eb96532b496ea8380ac02d8e4a97
wooseop <crazyz2000@naver.com>
2022-08-18 19:45:15 +0900

    Environment Setting.
    - pch
      - 프로젝트 속성 -> 사용
      - pch.cpp 파일 속성 -> 만들기
    
    - 라이브러리(ServerCore)를 Server와 Client에서 사용하기
      - 포함 디렉터리 : 파일 경로 추가
      - 라이브러리 디렉터리 : .lib 파일 경로 추가 (ServerCore 빌드 결과물 경로)

---
+++ DummyClient/main.cpp
@@ -0,0 +1,9 @@
+#include "pch.h"
+
+using namespace std;
+
+int main(void)
+{
+
+	return 0;
+}
---
+++ ServerCore/CorePch.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#include <iostream>
+
+#include <WinSock2.h>
+#include <MSWSock.h>
+#include <WS2tcpip.h>
+#pragma comment(lib, "ws2_32.lib")
